111acbe4ce7de43c04ec7dcf5ee387394369707d
Mayra Torres <isemayratorres18@gmail.com>
2023-01-25 21:19:02 -0500

    Listar alumnos - students component - Intercabio UK

--- Lista-Torres/src/app/components/students/students.component.ts
+++ Lista-Torres/src/app/components/students/students.component.ts
@@ -14,5 +14,59 @@ export class StudentsComponent {
     'Postulación',  /*Universidad de intercambio*/
     'Idioma',
     'Nota Promedio',
+    'Fecha de inicio',
   ];
+
+  rows=[
+    {
+      ID: '0',
+      Nombre: 'Matias Matus',
+      Postulacion: 'Universidad de Mánchester',
+      Idioma: 'Español',
+      Nota: '16',
+      Fecha_inicio: '01/02/2023',
+    },
+    {
+      ID: '1',
+      Nombre: 'Melani Munares',
+      Postulacion: 'Universidad de Birmingham',
+      Idioma: 'Español',
+      Nota: '19',
+      Fecha_inicio: '07/02/2023',
+    },
+    {
+      ID: '2',
+      Nombre: 'Mayra Torres',
+      Postulacion: 'Universidad de Oxford',
+      Idioma: 'Español',
+      Nota: '20',
+      Fecha_inicio: '08/02/2023',
+    },
+    {
+      ID: '3',
+      Nombre: 'Tomas Castro',
+      Postulacion: 'Universidad de Edimburgo',
+      Idioma: 'Español',
+      Nota: '17',
+      Fecha_inicio: '01/02/2023',
+    },
+    {
+      ID: '4',
+      Nombre: 'Isabella Vera',
+      Postulacion: 'Universidad de Leeds',
+      Idioma: 'Español',
+      Nota: '18',
+      Fecha_inicio: '10/02/2023',
+    },
+    {
+      ID: '5',
+      Nombre: 'Alexandra Castro',
+      Postulacion: 'Universidad de York',
+      Idioma: 'Español',
+      Nota: '20',
+      Fecha_inicio: '10/02/2023',
+    },
+
+  ];
+
 }
